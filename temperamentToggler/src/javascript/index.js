e351481e1713de91e47448a39558653ce6afd1f0
Neal Tucker <ntucker@august20th.com>
2020-04-04 15:25:31 -0700

    WEB PIANO

--- temperamentToggler/src/javascript/index.js
+++ temperamentToggler/src/javascript/index.js
@@ -4,6 +4,22 @@ import App from './components/App.jsx';
 const chordIdentifier = require('./chordIdentifier');
 const synth = require('./synthclient');
 window.synth = synth; // for using in console
+const MidiListener = require('./midilistener');
+
+const ml = new MidiListener();
+ml.addEventListener('midi', (e) => {
+  if(e.detail.data[0] == 144) {
+    console.log("noteOn");
+    // note on
+    synth.noteOn(0, e.detail.data[1], e.detail.data[2]);
+  } else if(e.detail.data[0] == 128) {
+    // note off
+    synth.noteOff(0, e.detail.data[1]);
+  } else {
+    console.log("unhandled midi: ", e.detail.data);
+  }
+});
+
 
 console.log('fark');
 ReactDOM.render(
